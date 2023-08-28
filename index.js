be9f58d12562a9aee377b4ff2dc35611216699bf
Seven <graf.lancelot7@gmail.com>
2023-08-28 14:19:49 +0300

    add proj

---
+++ index.js
@@ -0,0 +1,17 @@
+console.log( document.querySelector("[data-modal-close]"));
+(() => {
+    const refs = {
+      openModalBtn: document.querySelector("[data-modal-open]"),
+      closeModalBtn: document.querySelector("[data-modal-close]"),
+      modal: document.querySelector("[data-modal]"),
+      body: document.querySelector("[lock]"),
+    };
+  
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+  
+    function toggleModal() {
+      refs.modal.classList.toggle("is-hidden");
+      refs.body.classList.toggle("lock")
+    }
+  })();
